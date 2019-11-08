7894a9cd600a6bdbde71fd3d46dd26702334e608
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-08 13:07:07 -0500

    Spring initializr

---
+++ src/main/java/jbc/oct21/jindanupajit/staticreceipewebsite/StaticReceipeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package jbc.oct21.jindanupajit.staticreceipewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticReceipeWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticReceipeWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jbc/oct21/jindanupajit/staticreceipewebsite/StaticReceipeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package jbc.oct21.jindanupajit.staticreceipewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticReceipeWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
